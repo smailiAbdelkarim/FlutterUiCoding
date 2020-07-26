bbfb4abc83f860115ce6229a8d7decf5dfa75fdd
Haroki <haroki@localhost.localdomain>
2020-07-26 14:32:27 +0200

    add fontawesom icons and background color

--- lib/profiles/profile1/profile_1.dart
+++ lib/profiles/profile1/profile_1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutteruicodiing/profiles/profile1/user.dart';
 import 'provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class Profile1 extends StatefulWidget {
   @override
   _Profile1State createState() => _Profile1State();
@@ -8,11 +9,38 @@ class Profile1 extends StatefulWidget {
 
 class _Profile1State extends State<Profile1> {
   Profile profile = Profile1Provider.getProfile();
+
   @override
   Widget build(BuildContext context) {
-        return Stack(
-          children: <Widget>[
-            Image.asset('assets/profiles/1.jpeg'),
-          ],
-        );
+    return Stack(
+      children: <Widget>[
+        Image.asset('assets/profiles/1.jpeg',
+           fit: BoxFit.cover,
+           height: MediaQuery.of(context).size.height*0.45,
+           ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(title: Text("Portfolio"),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(FontAwesomeIcons.bars),
+              onPressed: (){},
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(FontAwesomeIcons.cog),
+                onPressed: (){},
+                )
+            ],
+
+
+          ),
+          body: Container(),
+        )
+      ],
+
+    );
+  }
 }
